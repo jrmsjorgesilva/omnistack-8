b0899aa0bff99933d4f64f7459b5766e83efa05c
jorge machado <jrms_jorgesilva@hotmail.com>
2019-08-07 02:18:41 -0300

    primeiro dia da omnistack concluido

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const DevController = require('./controllers/DevController');
+const LikeController = require('./controllers/LikeController');
+const DislikesController = require('./controllers/DislikeController');
+
+const routes = express.Router();
+
+//GET, POST, PUT, DELETE [MAIN METHODS]
+// routes.get('/', (req, res) => {
+//     return res.json({ message: `hello ${req.query.name}`});
+// });
+
+routes.post('/devs', DevController.store);
+routes.post('/devs/:devId/likes', LikeController.store);
+routes.post('/devs/:devId/dislikes', DislikeController.store);
+
+module.exports = routes;
---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routes = require('./routes');
+
+const server = express();
+
+mongoose.connect(
+    'mongodb+srv://omnistack:omnistack@cluster0-lyopf.mongodb.net/test?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true
+    }
+);
+
+server.use(cors());
+server.use(express.json());
+server.use(routes);
+
+server.listen(3000);
